683a29918b7e5f8a563067a751a1a56f25f0af51
Geordano Polanco <geordano.pr@gmail.com>
2025-12-21 18:24:35 +0100

    feat: initialize project with Jest testing, shadcn/ui components, and updated global styling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    "tests/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
